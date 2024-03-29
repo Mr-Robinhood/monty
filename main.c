50a819730cb08edad4292f890b1380c39d58df08
Mr-Robinhood <donomer98@gmail.com>
2024-03-29 03:47:06 +0200

    task 1

--- main.c
+++ main.c
@@ -1,33 +1,41 @@
 #include <stdio.h>
 #include "monty.h"
+bus_t bus = {NULL, NULL, NULL, -1};
+/**
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
+*/
 
-bus_t bus = {NULL, NULL, NULL, 0};
+int main(int argc, char *argv[])
+{
+	char content[1024];
+	FILE *file;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
 
-int main(int argc, char *argv[]) {
-    char content[1024]; 
-    FILE *file;
-    stack_t *stack = NULL;
-    unsigned int counter = 0;
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		return (EXIT_FAILURE);
+	}
 
-    if (argc != 2) {
-        fprintf(stderr, "USAGE: monty file\n");
-        return EXIT_FAILURE;
-    }
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		return (EXIT_FAILURE);
+	}
 
-    file = fopen(argv[1], "r");
-    bus.file = file;
-    if (!file) {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        return EXIT_FAILURE;
-    }
+	while (fgets(content, sizeof(content), file) != NULL)
+	{
+		counter++;
+		execute(content, &stack, counter, file);
+	}
 
-    while (fgets(content, sizeof(content), file) != NULL) {
-        counter++;
-        execute(content, &stack, counter, file);
-    }
-
-    free_stack(stack);
-    fclose(file);
-    return 0;
+	free_stack(stack);
+	fclose(file);
+	return (0);
 }
-
